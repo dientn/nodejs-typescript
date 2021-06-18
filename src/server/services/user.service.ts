a602778a06f0dbccb746579cc9ceb83d3bf94ef6
dientran2991 <81892778+dientran2991@users.noreply.github.com>
2021-06-18 23:50:41 +0700

    update systax

--- src/server/services/user.service.ts
+++ src/server/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Service } from "typedi";
-import { IUser } from "../models/User";
+import { IUser } from "../models/user";
 import UserRepository from "../repositories/user.repository";
 
 @Service()
