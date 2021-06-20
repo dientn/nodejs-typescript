1cae000b8afa37f1a4248cd8b2c1f93493174951
dientran2991 <81892778+dientran2991@users.noreply.github.com>
2021-06-20 22:56:29 +0700

    update somethings

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [{
       name: "app",
-      script: "src/index.ts",
+      script: "npm start",
       watch       : true,
       env: {
         NODE_ENV: "development",
